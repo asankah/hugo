6b4c41215fefdcbf72352492a5f3655166ed4c1a
Asanka Herath <asanka@google.com>
2020-07-27 01:09:18 -0400

    Generalize some Pandoc options.

--- markup/pandoc/pandoc_config/pandoc.go
+++ markup/pandoc/pandoc_config/pandoc.go
@@ -1,5 +1,10 @@
 package pandoc_config
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Config contains configuration settings for Pandoc.
 type Config struct {
 	// Input format. Use the 'Extensions' field to specify extensions thereof.
@@ -40,6 +45,89 @@ type Config struct {
 	// --from=markdown+foo+bar on the pandoc commandline.
 	Extensions []string
 
-	// Random extra arguments.
+	// List of input format extensions to use. Specifying ["foo", "bar"] is
+	// equivalent to specifying --from=markdown+foo+bar on the pandoc commandline
+	// assuming InputFormat is "markdown".
+	InputExtensions []string
+
+	// List of output format extensions to use. Specifying ["foo", "bar"] is
+	// equivalent to specifying --to=html5+foo+bar on the pandoc commandline,
+	// assuming UseLegacyHTML is false. Invoke "pandoc --list-extensions=html5" to
+	// or "pandoc --list-extensions=html5" to see the list of extensions that can
+	// be specified here.
+	OutputExtensions []string
+
+	// Extra commandline options passed to the pandoc invocation. These options
+	// are appended to the commandline after the format and filter options.
+	// Arguments are passed in literally. Hence must have the "--" or "-" prefix
+	// where applicable.
 	ExtraArgs []string
+}
+
+func (c *Config) getInputArg() string {
+	var b strings.Builder
+	b.WriteString("--from=")
+	if len(c.InputFormat) > 0 {
+		b.WriteString(c.InputFormat)
+	} else {
+		b.WriteString("markdown")
+	}
+
+	for _, extension := range c.InputExtensions {
+		b.WriteString("+")
+		b.WriteString(extension)
+	}
+	return b.String()
+}
+
+func (c *Config) getOutputArg() string {
+	var b strings.Builder
+	b.WriteString("--to=")
+	if c.UseLegacyHtml {
+		b.WriteString("html")
+	} else {
+		b.WriteString("html5")
+	}
+
+	for _, extension := range c.OutputExtensions {
+		b.WriteString("+")
+		b.WriteString(extension)
+	}
+	return b.String()
+}
+
+func (c *Config) getMathRenderingArg() string {
+	switch {
+	case c.UseMathml:
+		return "--mathml"
+	case c.UseWebtex:
+		return "--webtex"
+	case c.UseKatex:
+		return "--katex"
+	default:
+		return "--mathjax"
+	}
+}
+
+func (c *Config) getFilterArgs() []string {
+	var args []string
+	for _, filter := range c.Filters {
+		args = append(args, fmt.Sprintf("--filter=%s", filter))
+	}
+	return args
+}
+
+// AsPandocArguments returns a list of strings that can be used as arguments to
+// a "pandoc" invocation. All the settings contained in Config are represented
+// in the returned list of arguments.
+func (c *Config) AsPandocArguments() []string {
+	args := []string{
+		c.getInputArg(),
+		c.getOutputArg(),
+		c.getMathRenderingArg()}
+
+	args = append(args, c.getFilterArgs()...)
+	args = append(args, c.ExtraArgs...)
+
+	return args
 }
